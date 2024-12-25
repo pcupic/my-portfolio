33164ec6cc7cc6a304b845d996dcc76beb76a51e
pcupic <cupicpatrik@gmail.com>
2024-12-25 15:09:49 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-red-600">Tailwind Setup</h1>
+      <p className='underline'>Styling jsx elements</p>
+    </div>
+  )
+}
+
+export default App
